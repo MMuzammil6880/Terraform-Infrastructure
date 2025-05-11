b750e4b8d50825c25cfb5cdfd802f45626cfe8bb
Muhammad Muzammil <mmuzammil6880@gmail.com>
2025-05-11 13:28:26 +0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,29 @@
-module "wordpress_server_key_pair" {
-  source = "terraform-aws-modules/key-pair/aws"
 
-  key_name           = "${var.env_prefix}-wordpress-server-key"
-  create_private_key = true
+# Generate private key
+resource "tls_private_key" "wordpress" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
+
+# Create AWS key pair from public key
+resource "aws_key_pair" "wordpress" {
+  key_name   = "wordpress-server-key"
+  public_key = tls_private_key.wordpress.public_key_openssh
+}
+
+# Save private key locally
+resource "local_file" "private_key" {
+  content         = tls_private_key.wordpress.private_key_pem
+  filename        = "${path.module}/wordpress-server-key.pem"
+}
+
+resource "null_resource" "set_pem_permissions" {
+  provisioner "local-exec" {
+    command = "chmod 400 ${local_file.private_key.filename}"
+  }
+  depends_on = [local_file.private_key]
+}
+
 
 
 resource "aws_eip" "wordpress" {
@@ -18,29 +38,41 @@ resource "aws_eip" "wordpress" {
 module "wordpress_server" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
-  ami = "ami-084568db4383263d4"
+  name = "${var.env_prefix}-wordpress-server"
+  ami = "ami-07d9b9ddc6cd8dd30"
   instance_type          = var.ec2_instance_type
-  key_name               = module.wordpress_server_key_pair.key_pair_name
+  key_name               = aws_key_pair.wordpress.key_name
   monitoring             = false 
-  vpc_security_group_ids = module.wordpress_sg.security_group_id
+  vpc_security_group_ids = [module.wordpress_sg.security_group_id]
   subnet_id              = aws_subnet.public1.id
-  associate_public_ip_address = false
-  iam_instance_profile = module.ssm_role.iam_instance_profile_name
+  associate_public_ip_address = true
+  iam_instance_profile = module.ssm_role.iam_instance_profile_arn
+
+    user_data = <<-EOF
+              #!/bin/bash
+              apt update && apt upgrade -y
+              apt install -y python3 python3-pip git ansible
+
+              # Clone Ansible repo
+              git clone https://github.com/MMuzammil6880/wodpress-ansible.git /opt/wordpress-ansible
+
+              # Run playbook
+              cd /opt/wordpress-ansible
+              ansible-playbook -i "localhost," -c local wordpress.yml
+              EOF
 
   tags = {
-    name    = "${var.env_prefix}-wordpress-server"
+    
     managed = "${var.tf_tag}"
   }
 
   depends_on = [
-    module.wordpress_server_key_pair,
+    aws_key_pair.wordpress,
     aws_eip.wordpress,
     module.ssm_role
   ]
 
 }
-
-
 
 
 resource "aws_eip_association" "wordpress_eip_association" {
@@ -53,3 +85,6 @@ resource "aws_eip_association" "wordpress_eip_association" {
     aws_eip.wordpress
    ]
 }
+
+
+
