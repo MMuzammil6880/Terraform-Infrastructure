02c2114b063e9749c2c12e98e3706d07961a7415
@muhammad.muzammil <muhammad.muzammil@sudoconsultants.com>
2025-06-02 12:57:06 +0500

    final push

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,16 @@
-# # This file contains the output variables for the prod environment.
+# This file contains the output variables for the prod environment.
 
-# # VPC ID
+# VPC ID
 
-# output "vpc_id" {
-#   value       = module.vpc.vpc_id
-#   description = "value of the Prod-VPC"
-# }
+output "vpc_id" {
+  value       = module.vpc.vpc_id
+  description = "value of the Prod-VPC"
+}
 
 
-# #wordpress server public IP
-# output "wordpress_server_public_ip" {
-#   value       = aws_eip.server.public_ip
-#   description = "value of the Prod-Wordpress-Public-IP"
-# }
+#wordpress server public IP
+output "wordpress_server_public_ip" {
+  value       = aws_eip.server.public_ip
+  description = "value of the Prod-Wordpress-Public-IP"
+}
 
